e535d1f8bd27f3830e140e2aea808ef5bd0a664d
alex-njuguna <njugunakinuthia013@gmail.com>
2024-07-26 23:24:49 +0300

    define profile and post models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,26 @@
+"""
+Post - author, title, body, liked, created, updated
+"""
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
-# Create your models here.
+from profiles.models import Profile
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=300, unique=True, blank=True)
+    body = models.TextField()
+    liked = models.ManyToManyField(User, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.title)
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,34 @@
+"""
+Profile - user, bio, avatar, created, updated
+"""
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from PIL import Image
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(default="default.jpg", upload_to="avatars")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.avatar:
+            img = Image.open(self.avatar.path)
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.avatar.path)
+
+    def __str__(self):
+        return f"Profile for {self.user.username}"
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+post_save.connect(create_profile, sender=User)
+
