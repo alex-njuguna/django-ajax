dbc7d9d553fc40c643c441737e7218e33a76d484
alex-njuguna <njugunakinuthia013@gmail.com>
2024-07-27 00:06:59 +0300

    display all post in the database to the html

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from .models import Post
+
 
 def home(request):
-
-    return render(request, "posts/home.html")
+    """query all posts from the database"""
+    posts = Post.objects.all().order_by("id")
+    context = {
+        "posts": posts
+    }
+    return render(request, "posts/home.html", context)
